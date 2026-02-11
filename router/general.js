ff30586fc334e27e0a70a30c647b720b6d6e8e17
mariodev01 <mariovalentinomata@gmail.com>
2026-02-10 22:33:27 -0400

    I finished the tasks 1 to 4

--- router/general.js
+++ router/general.js
@@ -13,25 +13,40 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "El Mario Bombaaaa!"});
+  res.send(JSON.stringify(books));
+
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = parseInt(req.params.isbn);
+
+  res.send(JSON.stringify(books[isbn]));
+  //return res.status(300).json({message: "Ayyy amorrrr, no se que tiene tu mirarrr"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+
+  let data = Object.entries(books)
+              .filter(([id,libro])=>libro.author === author);
+
+  res.send(JSON.stringify(data));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title.replace(/\s+/g, '');;
+
+  let data = Object.entries(books)
+              .filter(([id,libro])=>libro.title.replace(/\s+/g, '') === title);
+
+  res.send(JSON.stringify(data));
 });
 
 //  Get book review
